295815bf308f6fdd768c22b8b49b11b1a5becbc7
MacBook Air 2 <macbookair2@MacBooks-MacBook-Air-2.local>
2019-08-23 10:13:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/man.jpg'),
+              ),
+              Text(
+                'Sbo Mxinwa',
+                style: TextStyle(
+//                 fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ) ,
+              ),
+              Text(
+                'Android/IOS Developer',
+                style: TextStyle(
+//                  fontFamily: 'Source San Pro',
+                color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Colors.teal,
+              ),
+              title: Text('+44 123 456 789',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontSize: 20.0,
+                ),
+              ),
+            )
+        ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'sbo@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                )
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+
